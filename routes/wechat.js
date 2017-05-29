978f56ff2e46f3f86cc67d7d06ebf3e66cb38a6c
Haoyun Wen <haoyun_wen@hotmail.com>
2017-05-29 12:37:30 -0700

    debug

--- routes/wechat.js
+++ routes/wechat.js
@@ -9,7 +9,7 @@ router.get('/', function(req, res, next) {
   var url = client.getAuthorizeURL('http://wxgzhpaytest.maxtropy.com/wechat/openid', 'AUTH', 'snsapi_userinfo');
   request(url, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        console.log(response);
+        console.log(response.data);
           res.send(response.data);
        }
   })
